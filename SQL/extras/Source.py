b6db8bc0053aa0e828fb4f6045462dd77baf165e
KZR-WALI <105113575+KZR-WALI@users.noreply.github.com>
2022-05-08 00:06:36 +0300

    Add files via upload

---
+++ SQL/extras/Source.py
@@ -0,0 +1,11 @@
+import re
+import time
+from datetime import datetime
+from userbot import StartTime, iqthon
+from userbot.Config import Config
+from userbot.plugins import mention
+help1 = ("**☭⦙ كيفيه التنصيب :**")
+help2 = ("**☭⦙ قـائمـه الاوامـر :**\n**☭⦙ قنـاه السـورس :** @rxoxx** ")
+TG_BOT = Config.TG_BOT_USERNAME
+TM = time.strftime("%I:%M")
+Sour = f"**.𓄌 : version 7.5  𓇡.** \n.𓄌 : me  {mention}  𓇡. \n**.𓄌 : time  {TM}  𓇡.**\n**.𓄌 : My Bot {TG_BOT} 𓇡.**\n**.𓄌 : Source : @rxoxx  𓇡.**"
